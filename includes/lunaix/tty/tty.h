8a7b57907119cabb612daf65941ccf437d74d5fc
wcz123694578 <wcz123694578@outlook.com>
2024-04-10 05:21:45 +0800

    tty头文件

---
+++ includes/lunaix/tty/tty.h
@@ -0,0 +1,6 @@
+void tty_set_theme(unsigned short fg, unsigned bg);
+void tty_put_char(char chr);    // 输出字符
+void tty_put_str(char* str);    // 输出字符串
+void tty_scroll_up();           // 上滚
+void tty_clear();
+
