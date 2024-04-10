0d2cfc1091ae3e8620e1be8438029a4569eccc02
wcz123694578 <wcz123694578@outlook.com>
2024-04-10 18:25:37 +0800

    实现字符串写进显存

---
+++ kernel/kernel.c
@@ -0,0 +1,12 @@
+#include <lunaix/tty/tty.h>
+
+void _kernel_init() {
+    // TODO
+}
+
+void _kernel_main(void* info_table) {
+    // TODO
+    tty_set_theme(VGA_COLOR_GREEN, VGA_COLOR_BLACK);
+    tty_put_str("Hello kernel world!!\nffffff");
+    // TODO: 安装GDT、IDT
+}
